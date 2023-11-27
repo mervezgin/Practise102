f3f2dd828e245bfad8191f0d86afd01675209c91
Merve <mervezgin@gmail.com>
2023-11-27 15:00:46 +0300

    Trying Some Methods

---
+++ src/Practise102/Practise102.Api/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Practise102.Api.Models;
+using System.Runtime.CompilerServices;
+
+namespace Practise102.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private static List<User> users = new List<User> 
+        {
+            new User{Id = 1, Name = "Merve", LastName = "Azgın", Place = "Ankara"},
+            new User{Id = 2, Name = "Yiğit", LastName = "Adaş", Place = "Ankara"}
+        };
+        [HttpGet]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSingleUser(int id)
+        {
+            var singleUser = users.Find(x => x.Id == id);
+            if(singleUser is null)
+            {
+                return NotFound("Sorry, but this user doesn't exist.");
+            }
+            return Ok(singleUser);
+        }
+    }
+}
--- src/Practise102/Practise102.Api/Models/User.cs
+++ src/Practise102/Practise102.Api/Models/User.cs
@@ -4,7 +4,6 @@
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
-        public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public string Place { get; set; } = string.Empty;
     }
